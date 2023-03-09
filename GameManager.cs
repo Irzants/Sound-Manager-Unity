bea0ae342c472b6be30123e045ef8628b4c8334d
Irzan Tri Saputra <36060059+Irzants@users.noreply.github.com>
2023-03-09 16:56:01 +0700

    Create GameManager.cs

---
+++ GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RichGagak.Meowtyp
+{
+    public class GameManager : Singleton<GameManager>
+    {
+        void Start()
+        {
+            UIManager.Instance.loadViewFunc = LoadViewFunc;
+            Application.logMessageReceived += LogCallback;
+            SoundManager.Instance.Play("bgm_menu", true);
+        }
+
+        static ViewBase LoadViewFunc(string panelName)
+        {
+            return ResourceUtil.Load<ViewBase>(PathUtil.Panel(panelName));
+        }
+
+        static void LogCallback(string condition, string stackTrace, LogType type)
+        {
+
+        }
+
+        [RuntimeInitializeOnLoadMethod]
+        private static void RTInit()
+        {
+            Instance.Dummy();
+        }
+    }
+}
