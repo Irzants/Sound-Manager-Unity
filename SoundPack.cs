e08b6a7fdff8d5254a58ad509ac8aa4b9f07827d
Irzan Tri Saputra <36060059+Irzants@users.noreply.github.com>
2023-05-19 16:47:04 +0700

    Update SoundPack.cs

--- SoundPack.cs
+++ SoundPack.cs
@@ -2,11 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace RichGagak.Meowtyp
-{
     [CreateAssetMenu(fileName = "Data_Pack_Sound", menuName = "Rich Gagak/Audio/Sound Pack")]
     public class SoundPack : ScriptableObject
     {
         public List<SoundManager.SoundInfo> soundList;
     }
-}
